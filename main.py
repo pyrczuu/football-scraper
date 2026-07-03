1ebe68dd3b1809deb9ebdfc0472867b6a898e7e5
Damian <dydekpyrcz@gmail.com>
2026-07-03 20:00:01 +0200

    extracting stats

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
+from pandas.core.interchange.dataframe_protocol import DataFrame
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 load_dotenv()
 
@@ -15,13 +17,17 @@ def crawl(path: str):
         categoryNames = driver.find_elements(By.CSS_SELECTOR, 'h3[class*=TLStatsTopThreeHeader')
         categories = zip(categoryNames, categoryLinks)
         for category in categories:
-            collect(category[0], category[1])
+            collect(driver, category[0], category[1])
     finally:
         driver.quit()
 
-def collect(name, link):
-    pass
-
+def collect(driver, name, link):
+    driver.get(link)
+    players = driver.find_elements(By.CSS_SELECTOR, 'div[class*=LeagueSeasonStatsTableCSS a')
+    for player in players:
+        name = driver.find_element(By.CSS_SELECTOR, 'span[class*=TeamOrPlayerName')
+        value = driver.find_element(By.CSS_SELECTOR, 'span[class*=StatValue')
+        # add saving logic, probably straight to postgres, no excel as middle man
 def main():
     SOURCE = os.getenv("SOURCE")
     OUTPUT_PATH = os.getenv("OUTPUT_PATH")
