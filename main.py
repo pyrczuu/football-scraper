4e3d9ed34baf13f36d37805a7113a37f8c030052
Damian <dydekpyrcz@gmail.com>
2026-07-04 13:23:28 +0200

    cookies done, categories done
    TODO: collecting individual stats

--- main.py
+++ main.py
@@ -4,35 +4,59 @@ from pandas.core.interchange.dataframe_protocol import DataFrame
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
+from selenium.webdriver.support.wait import WebDriverWait
+import logging
 
 load_dotenv()
+logger = logging.getLogger()
+SOURCE = os.getenv("SOURCE")
+STATS_PAGE = os.getenv("STATS_PAGE")
+OUTPUT_PATH = os.getenv("OUTPUT_PATH")
 
 
 def crawl(path: str):
     driver = webdriver.Chrome()
 
+
+    cookies_selector = 'button.fc-cta-consent.fc-primary-button'
     driver.get(path)
+    driver.wait = WebDriverWait(driver, 5)
+
+    cookies = driver.find_elements(By.CSS_SELECTOR, cookies_selector)
+    if len(cookies) > 0:
+        cookies[0].click()
+        logger.info("Cookies clicked")
     try:
-        categoryLinks = driver.find_elements(By.CSS_SELECTOR, 'div[class*="TLStatsTopThreeHeader"] a')
-        categoryNames = driver.find_elements(By.CSS_SELECTOR, 'h3[class*=TLStatsTopThreeHeader')
-        categories = zip(categoryNames, categoryLinks)
+        elements = driver.find_elements(By.CSS_SELECTOR, 'div[class*=TLStatsTopThreeCSS] > a')
+        category_links = [el.get_attribute('href') for el in elements]
+        category_names = driver.find_elements(By.CSS_SELECTOR, 'h3[class*=TLStatsTopThreeHeader')
+        categories = zip(category_names, category_links)
+        logger.info(f"Found {len(category_links)} links and {len(category_names)} names")
+        print(category_links)
         for category in categories:
+            logger.info(f"Visiting {category[1]}")
             collect(driver, category[0], category[1])
     finally:
+        logger.info("Closing driver")
         driver.quit()
 
-def collect(driver, name, link):
+def collect(driver, category, link):
     driver.get(link)
     players = driver.find_elements(By.CSS_SELECTOR, 'div[class*=LeagueSeasonStatsTableCSS a')
-    for player in players:
-        name = driver.find_element(By.CSS_SELECTOR, 'span[class*=TeamOrPlayerName')
-        value = driver.find_element(By.CSS_SELECTOR, 'span[class*=StatValue')
-        # add saving logic, probably straight to postgres, no excel as middle man
-def main():
-    SOURCE = os.getenv("SOURCE")
-    OUTPUT_PATH = os.getenv("OUTPUT_PATH")
+    with open('test.txt', 'a') as f:
+        f.write(f"player | {category}")
 
-    crawl(SOURCE)
+        for player in players:
+            name = driver.find_element(By.CSS_SELECTOR, 'span[class*=TeamOrPlayerName')
+            value = driver.find_element(By.CSS_SELECTOR, 'span[class*=StatValue')
+
+            # temp solution for testing
+            f.write(f"{name}; {value}\n")
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    crawl(SOURCE+STATS_PAGE)
 
 if __name__ == '__main__':
     main()
