c789e102d202306e5e36eabe4824cce3055083c1
Damian <dydekpyrcz@gmail.com>
2026-07-24 22:45:15 +0200

    add regex and selectors for player

--- player_profile.py
+++ player_profile.py
@@ -1,5 +1,16 @@
-def save_if_absent(connection, logger, path: str):
-    player_id = "" #TODO extract from url
+import re
+
+from selenium.webdriver.common.by import By
+
+
+def save_if_absent(connection, driver, logger, url: str):
+    player_id = re.search(r'/players/(\d+)', url)
+    player_name = driver.find_element(By.CSS_SELECTOR, 'h1[class*=PlayerNameCSS]').text.strip()
+    player_club = driver.find_element(By.CSS_SELECTOR, 'div[class*=TeamCSS]').text.strip()
+    player_nation = driver.find_element(By.CSS_SELECTOR, 'a[class*=CountryLinkCSS]').text.strip()
+    player_age = "" #TODO, tricky, maybe findselemnts and get by index from list
+    player_position = driver.find_element(By.CSS_SELECTOR, 'div[class*=PositionCSS]').text.strip()
+    player_value = ""  #TODO
 
     try:
         cursor = connection.cursor()
